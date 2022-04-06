15c72b92d3e521e269951370cde91740d6d71684
rockwillck <38663064+rockwillck@users.noreply.github.com>
2022-04-05 21:50:34 -0500

    Made chevron slide down, took FOREVER with safari compatability

--- js/effects.js
+++ js/effects.js
@@ -12,3 +12,36 @@ let key = setInterval(() => {
         done = true
     }
 }, 150)
+
+let speed = 35
+window.onload = function() {
+    var anchors = document.getElementsByTagName('button');
+    for(var i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i];
+        if (anchor.innerText.includes("..")) {
+            href = anchor.innerText.split("..")[2]
+            anchor.innerText = anchor.innerText.split("..")[1]
+        }
+        anchor.onclick = function() {
+            if (href.startsWith("\#")) {
+                let time = 0
+                let key2 = setInterval(() => {
+                    if (time < (document.getElementById(href.replace("\#", "").trim()).getBoundingClientRect().top - anchor.getBoundingClientRect().top + window.innerHeight/2 - speed)/speed) {
+                    window.scrollBy({
+                        top: speed, 
+                        left: 0, 
+                        behavior: 'smooth' 
+                      });
+                      time ++
+                    } else {
+                        if (!window.location.includes(href)) {
+                            window.location = window.location + href
+                        }
+                        clearInterval(key2)
+                    }
+                }, 1)
+                
+            }
+        }
+    }
+}
